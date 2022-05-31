e51389dfe43dc0cd55d3645c9cc072cbe1821246
Smooth1884 <smooth1884@gmail.com>
2022-05-31 11:36:11 +0800

    fixed bugy import

--- packages/backend/src/utils/tokenhandler.ts
+++ packages/backend/src/utils/tokenhandler.ts
@@ -1,6 +1,6 @@
 import _ from "lodash";
 import { DateTime } from "luxon";
-import { SwappableTokens } from "src/settings";
+import { SwappableTokens } from "../settings";
 
 import {
   AddressMap,
