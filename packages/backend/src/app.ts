5e35e2b7fc5c1ac29f55f471b5a27e1efd3c95e4
BigMike <peter@flynn.network>
2022-06-26 17:37:49 -0700

    Resolve conflict

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -69,7 +69,7 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
   // Express Static just loves to cache, but this stops it.
-  res.set("Cache-Control", "public, max-age=600");
+  res.set("Cache-Control", "no-store");
   next();
 });
 
